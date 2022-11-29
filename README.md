377c3664cdc41d8dc9c8b5153ceaab8b8f27edaf
AdnenMess <adnen_messaoudi@hotmail.com>
2022-11-29 16:22:43 +0100

    return error

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
-## FastAPI
+# FastAPI
 
 
-We have to install `fastapi` and `uvicorn(light web server)` 
+We have to install `fastapi` and `uvicorn(light web server)`
+
 ```shell
 pip install fastapi
 pip install uvicorn
 ```
 ***
+
 1-  **A Minimal API endpoint**
 
 ```python
@@ -22,3 +24,26 @@ def calculate():
 
 uvicorn.run(api, host="127.0.0.1", port=8000)
 ```
+***
+
+2- **Retuning errors**
+
+```python
+import fastapi
+from typing import Optional
+
+api = fastapi.FastAPI()
+
+@api.get('/api/calculate')
+def calculate(x: int,y: int,z : Optional[int] = None):
+    error_dict = {'error' : 'z cannot be zero'}
+    if z is not None and z == 0:
+        return fastapi.responses.JSONResponse(
+                       status_code=400, content=error_dict)
+    result = x + y
+    if z is not None:
+        result /= z
+    return result
+```
+***
+
--- main.py
+++ main.py
@@ -1,13 +1,38 @@
-from fastapi import FastAPI
+from typing import Optional
+
+import fastapi
 import uvicorn
 
-api = FastAPI()
+
+api = fastapi.FastAPI()
+
+
+@api.get('/')
+def index():
+    body = "<html>" \
+           "<body style='padding: 10px;'>" \
+           "<h1>Welcome to the API</h1>" \
+           "<div>" \
+           "Try it: <a href='/api/calculate?x=7&y=11'>/api/calculate?x=7&y=11</a>" \
+           "</div>" \
+           "</body>" \
+           "</html>"
+
+    return fastapi.responses.HTMLResponse(content=body)
 
 
 @api.get('/api/calculate')
-def calculate():
-    value = 2+2
-    result = {'result': value}
+def calculate(x: int, y: int, z: Optional[int] = None):
+    if z == 0:
+        return fastapi.responses.JSONResponse(content={'ERROR': 'z cannot be zero'}, status_code=400)
+
+    value = x + y
+    if z is not None:
+        value /= z
+    result = {'x': x,
+              'y': y,
+              'z': z,
+              'result': value}
     return result
 
 
