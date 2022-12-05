8b19c55f6410f0e31810e52b6693ec9da6207206
AdnenMess <adnen_messaoudi@hotmail.com>
2022-12-05 12:11:09 +0100

    weather_api final

---
+++ services/openweather_service.py
@@ -0,0 +1,26 @@
+from typing import Optional
+
+import httpx
+from pathlib import Path
+import json
+
+file = Path.cwd() /'services' / "key.json"
+with open(file, "r") as f:
+    read = json.load(f)
+    api_key = read.get("api-key")
+
+async def get_report_async(city: str, state: Optional[str], country: str, units: Optional[str]) -> dict:
+    if state:
+        q = f'{city},{state},{country}'
+    else:
+        q = f'{city},{country}'
+
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={q}&appid={api_key}'
+
+    async with httpx.AsyncClient() as client:
+        resp = await client.get(url)
+        resp.raise_for_status()
+
+    data = resp.json()
+    forecast = data['main']
+    return forecast
